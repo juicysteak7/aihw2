ad41583e6e81618ad80f1a286baad72aa168ecce
juicysteak7 <pedro.gonzalez6461@gmail.com>
2023-10-24 18:08:13 -0700

    changes

--- src/main.rs
+++ src/main.rs
@@ -3,84 +3,220 @@ use std::io::{self, BufRead};
 use std::env;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        eprintln!("Please only use one argument of the bugrush board's file name.");
-        std::process::exit(1);
-    }
-    let file_name: &str = &args[1];
+    // let args: Vec<String> = env::args().collect();
+    // if args.len() != 2 {
+    //     eprintln!("Please only use one argument of the bugrush board's file name.");
+    //     std::process::exit(1);
+    // }
+    // let file_name: &str = &args[1];
 
-    // Code to read a game board from a file
-    let mut game_board: Vec<Vec<char>> = Vec::new();
-    match read_board(file_name) {
-        Ok(result) => {
-            game_board = result;
+    // // Code to read a game board from a file
+    // let mut game_board: Vec<Vec<char>> = Vec::new();
+    // match read_board(file_name) {
+    //     Ok(result) => {
+    //         game_board = result;
+    //     }
+    //     Err(error) => {
+    //         eprintln!("{}", error);
+    //     }
+    // }
+
+    // if game_board.len() != 4 {
+    //     error("Game board not imported correctly");
+
+    // } else if game_board[0].len() != 4 {
+    //         error("Game board not imported correctly");
+    // }
+
+    // // find whos turn it is
+    // let mut xcount = 0;
+    // let mut ocount = 0;
+    // let player;
+    // let previous_player;
+    // for r in 0..4 {
+    //     for c in 0..4 {
+    //         if game_board[r][c] == 'X' {
+    //             xcount += 1;
+    //         } else if game_board[r][c] == 'O' {
+    //             ocount += 1;
+    //         }
+    //     }
+    // }
+    // if xcount == ocount {
+    //     player = 'X';
+    //     previous_player = 'O';
+    // } else {
+    //     player = 'O';
+    //     previous_player = 'X';
+    // }
+    // // print_board(game_board.clone());
+    // let result = recurse_board(game_board.clone(), player, previous_player,4);
+    // println!("{} {}",player, result);
+
+    let mut game_board = vec![vec!['.'; 4]; 4];
+    let previous_player = 'X';
+    let player = 'O';
+    print_board(game_board.clone());
+    while !is_complete(game_board.clone()) {
+        // Create a mutable string to store the user's input
+        let mut input = String::new();
+
+        // Print a prompt to the user
+        println!("Please enter your move (Example: 3 2)");
+
+        // Read user input and store it in the 'input' string
+        io::stdin().read_line(&mut input).expect("Failed to read line");
+
+        // Split the input into words and collect them into a Vec
+        let words: Vec<&str> = input.split_whitespace().collect();
+
+        // Ensure we have exactly two words
+        if words.len() != 2 {
+            println!("Please enter exactly two numbers separated by a space.");
+            return;
         }
-        Err(error) => {
-            eprintln!("{}", error);
-        }
-    }
 
-    if game_board.len() != 4 {
-        error("Game board not imported correctly");
-
-    } else if game_board[0].len() != 4 {
-            error("Game board not imported correctly");
-    }
-
-    // find whos turn it is
-    let mut xcount = 0;
-    let mut ocount = 0;
-    let player;
-    let previous_player;
-    for r in 0..4 {
-        for c in 0..4 {
-            if game_board[r][c] == 'X' {
-                xcount += 1;
-            } else if game_board[r][c] == 'O' {
-                ocount += 1;
+        // Parse the words into usize variables
+        let num1: usize = match words[0].parse() {
+            Ok(n) => n,
+            Err(_) => {
+                error("Invalid input for the first number.");
             }
+        };
+
+        let num2: usize = match words[1].parse() {
+            Ok(n) => n,
+            Err(_) => {
+                error("Invalid input for the second number.");
+            }
+        };
+
+        if num1 >= 4 || num2 >= 4 {
+            error("Please use numbers 0-3");
         }
+
+        if game_board[num1][num2] == '.'{
+            game_board[num1][num2] = 'X';
+        } else {
+            error("Please only play on empty spaces.");
+        }
+
+        // Negamax of depth4
+        game_board = take_step(game_board.clone(), player, previous_player, 10);
+
+        print_board(game_board.clone());
     }
-    if xcount == ocount {
-        player = 'X';
-        previous_player = 'O';
-    } else {
-        player = 'O';
-        previous_player = 'X';
-    }
-    // print_board(game_board.clone());
-    let result = recurse_board(game_board.clone(), player, previous_player);
-    println!("{} {}",player, result);
+    let result = evaluate_board(game_board.clone(), 'X');
+    println!("Your result: {}", result);
 }
 
-fn recurse_board(board: Vec<Vec<char>>, player: char, previous_player: char) -> i32 {
-    let mut result1:i32;
-    let mut result3 = i32::MIN;
-    let mut finished = true;
+// Recurse board using negamax of depth+1, then evaluate next move.
+fn take_step(board: Vec<Vec<char>>, player: char, previous_player: char, depth: usize) -> Vec<Vec<char>> {
+    //let new_board: Vec<Vec<char>> = Vec::new();
+    let mut result:i32;
+    let mut result2:i32 = i32::MIN;
+    let mut r_board:Vec<Vec<char>> = board.clone();
     for r in 0..4 {
         for c in 0..4 {
             // Empty spot, add it to possible moves
             if board[r][c] == '.' {
-                finished = false;
                 let mut new_board = board.clone();
-                new_board[r][c] = previous_player;
+                new_board[r][c] = player;
                 //print_board(new_board.clone());
                 if previous_player == 'X' {
-                    result1 = recurse_board(new_board, player, 'O');
+                    result = recurse_board(new_board.clone(), player, 'O', depth, 4, -4);
                 } else {
-                    result1 = recurse_board(new_board, player, 'X');
+                    result = recurse_board(new_board.clone(), player, 'X', depth, 4, -4);
                 }
-                if result1 > result3 {
-                    result3 = result1;
+                if result > result2 {
+                    result2 = result;
+                    r_board = new_board.clone();
                 }
             }
         }
     }
-    if finished {
-        return evaluate_board(board.clone(),player);
+    r_board
+}
+
+fn is_complete(board: Vec<Vec<char>>) -> bool {
+    for r in 0..4 {
+        for c in 0..4 {
+            if board[r][c] == '.' {
+                return false;
+            }
+        }
     }
-    result3
+    true
+}
+
+fn recurse_board(board: Vec<Vec<char>>, player: char, previous_player: char, depth: usize, mut alpha: i32, beta:i32) -> i32 {
+    // let mut result1:i32;
+    // let mut result2 = i32::MIN;
+    // let mut finished = true;
+    // if depth != 0 {
+    //     for r in 0..4 {
+    //         for c in 0..4 {
+    //             // Empty spot, add it to possible moves
+    //             if board[r][c] == '.' {
+    //                 finished = false;
+    //                 let mut new_board = board.clone();
+    //                 new_board[r][c] = previous_player;
+    //                 //print_board(new_board.clone());
+    //                 if previous_player == 'X' {
+    //                     result1 = recurse_board(new_board, player, 'O', depth-1);
+    //                 } else {
+    //                     result1 = recurse_board(new_board, player, 'X', depth-1);
+    //                 }
+    //                 if result1 > result2 {
+    //                     result2 = result1;
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+    // if finished || depth == 0 {
+    //     return evaluate_board(board.clone(),player);
+    // }
+    // result2
+
+    // Alpha beta
+    let mut result2 = i32::MIN;
+    let mut finished = true;
+
+    if depth != 0 {
+        for r in 0..4 {
+            for c in 0..4 {
+                if board[r][c] == '.' {
+                    finished = false;
+                    let mut new_board = board.clone();
+                    new_board[r][c] = previous_player;
+
+                    let result1 = -recurse_board(
+                        new_board.clone(),
+                        player,
+                        if previous_player == 'X' { 'O' } else { 'X' },
+                        depth - 1,
+                        -beta,
+                        -alpha,
+                    );
+
+                    result2 = result2.max(result1);
+                    alpha = alpha.max(result1);
+
+                    if alpha >= beta {
+                        //println!("pruned");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    if finished || depth == 0 {
+        return evaluate_board(board, player);
+    }
+
+    result2
 }
 
 fn read_board(filename: &str) -> io::Result<Vec<Vec<char>>> {
@@ -105,25 +241,45 @@ fn evaluate_board(board: Vec<Vec<char>>, player: char) -> i32 {
     for r in 0..4 {
         for c in 0..4 {
             // Check diagonals
-            if r > 2 && c <= 2 {
-                let mut diagonal1 = Vec::new();
-                for i in 0..3 {
-                    if c + i < 4 {
-                       diagonal1.push(board[r - i][c + i]);
-                    }
-                }
+            // if r > 2 && c <= 2 {
+            //     let mut diagonal1 = Vec::new();
+            //     for i in 0..3 {
+            //         if c + i < 4 {
+            //            diagonal1.push(board[r - i][c + i]);
+            //         }
+            //     }
+            //     if diagonal1.iter().all(|&c| c == 'X') {
+            //         xscore += 3;
+            //     } else if diagonal1.iter().all(|&c| c == 'O') {
+            //         oscore += 3;
+            //     }
+            // }
+
+            // if r > 2 && c >= 2 {
+            //     let mut diagonal2 = Vec::new();
+            //     for i in 0..3 {
+            //         diagonal2.push(board[r - i][c - i]);
+            //     }
+            //     if diagonal2.iter().all(|&c| c == 'X') {
+            //         xscore += 3;
+            //     } else if diagonal2.iter().all(|&c| c == 'O') {
+            //         oscore += 3;
+            //     }
+            // }
+
+            // Check the diagonal from the top-left corner to the bottom-right corner
+            if r + 3 < 4 && c + 3 < 4 {
+                let diagonal1: Vec<char> = (0..4).map(|i| board[r + i][c + i]).collect();
                 if diagonal1.iter().all(|&c| c == 'X') {
                     xscore += 3;
                 } else if diagonal1.iter().all(|&c| c == 'O') {
                     oscore += 3;
                 }
             }
-
-            if r > 2 && c >= 2 {
-                let mut diagonal2 = Vec::new();
-                for i in 0..3 {
-                    diagonal2.push(board[r - i][c - i]);
-                }
+            
+            // Check the diagonal from the top-right corner to the bottom-left corner
+            if r + 3 < 4 && c >= 3 {
+                let diagonal2: Vec<char> = (0..4).map(|i| board[r + i][c - i]).collect();
                 if diagonal2.iter().all(|&c| c == 'X') {
                     xscore += 3;
                 } else if diagonal2.iter().all(|&c| c == 'O') {
@@ -182,14 +338,14 @@ fn evaluate_board(board: Vec<Vec<char>>, player: char) -> i32 {
     }
 }
 
-// fn print_board(board: Vec<Vec<char>>){
-//     for row in board {
-//         for c in row {
-//             print!("{}",c);
-//         }
-//         println!();
-//     }
-// }
+fn print_board(board: Vec<Vec<char>>){
+    for row in board {
+        for c in row {
+            print!("{}",c);
+        }
+        println!();
+    }
+}
 
 // Print a usage error message and exit.
 fn error(e: &str) -> ! {
